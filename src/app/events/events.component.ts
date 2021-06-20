7e704b6a75ba553198a3b3c22845029f650a9684
Patricio <67854017+triciopa@users.noreply.github.com>
2021-06-20 11:01:08 -0300

    Input, focus & blur

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -7,12 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+  message: string;
+
+  constructor() {
+    this.message = 'This is the initial message';
+   }
 
   ngOnInit(): void {
   }
 
-  onClick() {
-    console.log('Button clicked')
+  onClick($event) {
+    console.log($event)
   }
-}
+
+  onChange($event) {
+    console.log($event.target.value)
+  }
+
+  onMouseEnter() {
+    // console.log('Entered')
+    this.message = `I'm in`;
+  }
+
+  onMouseOut() {
+    this.message = `I'm out`;
+  }
+
+  onChangeInput($event) {
+    console.log($event.target.value)
+  }
+
+  onInput($event) {
+    this.message = $event.target.value
+  }
+
+  onFocus() {
+    console.log('On focus')
+  }
+   
+  onBlur() {
+    console.log('Out of focus')
+  }
+};
