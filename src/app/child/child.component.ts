04feb046b7bc996965d70ff9f3009362bd99b1ba
Patricio <67854017+triciopa@users.noreply.github.com>
2021-06-22 20:05:09 -0300

    ngIf, ngFor, ngSwitch

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -10,6 +10,9 @@ export class ChildComponent implements OnInit {
   parProp: any;
   classProp: string[];
   selectedClass: string;
+  showP: boolean;
+  people: any[];
+  optionNumber: number;
 
   constructor() {
     this.parProp = {
@@ -22,6 +25,18 @@ export class ChildComponent implements OnInit {
     this.classProp.push('transform');
 
     this.selectedClass = '';
+
+    this.showP = false;
+
+    this.people = [
+      { fname: 'John', lname: 'Bonachon', age: 37, address: 'Rivadavia 4900 PB A', phone: '+5491136363636'},
+      { fname: 'Mary', lname: 'Jane', age: 12, address: 'Scalabritney Ortiz 2011', phone: '+5491112121212'},
+      { fname: 'Paul', lname: 'Bazo', age: 88, address: 'Malabia 99 4to C', phone: '+5491188888888'},
+      { fname: 'Charles', lname: 'Chaplin', age: 24, address: 'Hidalgo 1024', phone: '+5491124242424'},
+      { fname: 'George', lname: 'Harrison', age: 49, address: 'Av. Libertador 992 17mo F', phone: '+5491149494949'}
+    ];
+
+    this.optionNumber = 1;
    }
 
   ngOnInit(): void {
@@ -73,9 +88,13 @@ export class ChildComponent implements OnInit {
     }
   }
 
-    onRange($event: any) {
-      this.parProp.fontSize = `${$event.target.value}px`
-    }
-  
+  onRange($event: any) {
+    this.parProp.fontSize = `${$event.target.value}px`
+  }
 
+  showText() {
+    
+    this.showP = !this.showP;
+  
+  }
 }
